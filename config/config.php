1228f3eaeec29f86edcfd99508e45de07b17bee1
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-01 11:06:36 +0100

    Add configuration

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-	'app_key' => '',
-	'app_secret' => '',
-	'encryption_key' => '',
-	'root' => 'sandbox',
+	'app_key' => 'kp99xoktm9q0u0r',
+	'app_secret' => 'i9kkeiojpp15jv0',
+	'encryption_key' => '2f13cf3ed6872967f657df242e571893',
+	'root' => 'dropbox',
 );
